b5f9195798dee0dac17efa7bc4624ec7b746af4b
Aras Ekinci <me@arasekinci.com>
2026-08-20 16:45:20 +0000

    feat(react-hooks): export useUncontrolled from Mantine

--- packages/react-hooks/src/index.ts
+++ packages/react-hooks/src/index.ts
@@ -3,9 +3,12 @@ export type {
   UseDisclosureOptions,
   UseDisclosureReturnValue,
   UseMediaQueryOptions,
+  UseUncontrolledOptions,
+  UseUncontrolledReturnValue,
 } from "@mantine/hooks";
 export {
   useDisclosure,
   useMediaQuery,
   useMergedRef,
+  useUncontrolled,
 } from "@mantine/hooks";
